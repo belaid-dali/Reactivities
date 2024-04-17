4f70391733f04236a3c0625d65a63ea017a71c27
belaid-dali <daliomar.belaid@gmail.com>
2024-04-17 08:44:59 +0100

    Adding a NavBar compent to our UI

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import './styles.css';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar.tsx';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -18,7 +19,7 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities' />
+      <NavBar />
       <List>
         {activities.map((activity) => (
           <List.Item key={activity.id}>
